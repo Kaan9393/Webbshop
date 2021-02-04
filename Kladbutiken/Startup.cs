c749bbbb183ca51093d4865e76a264c1e691e03e
Pontus Gillson <pontus93g@gmail.com>
2021-02-04 10:35:46 +0100

    Update Startup.cs
    Ändrat namnet

--- Kladbutiken/Startup.cs
+++ Kladbutiken/Startup.cs
@@ -27,7 +27,7 @@ namespace Kladbutiken
             services.AddRazorPages();
             // Add more services
             // Test Kaan
-            // Test Pontus
+            // Test Marcus (Ändrat av Pontus)
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
