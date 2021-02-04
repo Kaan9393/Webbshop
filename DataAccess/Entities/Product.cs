0f3466172f45b60dc0289c9184a5247869c3c82a
arkestal <71063982+arkestal@users.noreply.github.com>
2021-02-04 15:09:32 +0100

    Update Product.cs
    Updated product model

--- DataAccess/Entities/Product.cs
+++ DataAccess/Entities/Product.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace DataAccess.Entities
 {
-    class Product
+    public class Product
     {
+        public int ID { get; set; }
+        public Category CategoryID { get; set; }
+
+        [DataType(DataType.Currency)]
+        public double Price { get; set; }
+        public int StockBalance { get; set; }
+        public double? Discount { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public int Sales { get; set; }
     }
 }
