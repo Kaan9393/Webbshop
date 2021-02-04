fad7443ebac021305c5fab4bd1f0991b3014f199
Tonny <tonny035@hotmail.com>
2021-02-04 09:30:55 +0100

    add comment

--- Kladbutiken/Startup.cs
+++ Kladbutiken/Startup.cs
@@ -24,6 +24,7 @@ namespace Kladbutiken
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            // Add more services
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
