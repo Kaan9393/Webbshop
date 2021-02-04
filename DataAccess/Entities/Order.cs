b6f533df8e02e729f50946e92fab3e1bb247e546
Johannes Hellman <johanneshellman@hotmail.com>
2021-02-04 12:13:07 +0100

    Update Order.cs
    Order Class fixed

--- DataAccess/Entities/Order.cs
+++ DataAccess/Entities/Order.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Collections;
 
 namespace DataAccess.Entities
 {
     class Order
     {
+        public int ID { get; set; }
+        public User UserID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public IList<Product> ProductList { get; set; }
     }
 }
