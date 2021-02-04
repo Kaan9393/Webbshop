c0b2804a93ac2610fa6ad6ec0564117cf2857be8
Kaan9393 <71064062+Kaan9393@users.noreply.github.com>
2021-02-04 09:44:21 +0100

    testKaan

--- Kladbutiken/Startup.cs
+++ Kladbutiken/Startup.cs
@@ -25,6 +25,7 @@ namespace Kladbutiken
         {
             services.AddRazorPages();
             // Add more services
+            // Test Kaan
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
