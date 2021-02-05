de6ad601751e81eb2949806f84b4e1a1d19679dd
Rickard-Kujala <rickard.kujala89@gmail.com>
2021-02-05 14:12:07 +0100

    Tables Update 2

--- DataAccess/Data/MainContext.cs
+++ DataAccess/Data/MainContext.cs
@@ -16,6 +16,10 @@ namespace DataAccess.Data
         public DbSet<Order> Orders { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasIndex(x => x.EmailAddress).IsUnique();
+        }
 
         public MainContext(DbContextOptions options) : base(options)
         {
--- DataAccess/Entities/User.cs
+++ DataAccess/Entities/User.cs
@@ -12,12 +12,16 @@ namespace DataAccess.Entities
         [MaxLength(13,ErrorMessage ="SSN must be 12 characters YYYY-MMDDXXXX"), MinLength(13)]
         public string SSN { get; set; }// Lagt till personnummer
         [Required]
+        [MinLength(8,ErrorMessage ="Password must be atleast 8 characters and less than 50 "),MaxLength(50)]
+        public string Password { get; set; }
+        [Required]
         [MaxLength(50)]
         public string FirstName { get; set; }
         [Required]
         [MaxLength(50)]
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }// Ändrade till string
+        [Required]
         public string EmailAddress { get; set; }// länka till address ?
         public DateTime RegisterDate { get; set; }
         public IList<Product> ProductCart { get; set; }
