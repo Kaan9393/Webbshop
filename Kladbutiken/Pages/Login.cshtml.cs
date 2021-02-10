f41ca2dd9cb532d497a2e1d0ed97f47d1222c0ca
Tonny <tonny035@hotmail.com>
2021-02-10 14:18:55 +0100

    Login with cookie

--- Kladbutiken/Pages/Login.cshtml.cs
+++ Kladbutiken/Pages/Login.cshtml.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DataAccess.Models;
+using DataAccess.Repositories;
 
 namespace Kladbutiken.Pages
 {
     public class LoginModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public UserLoginModel UserLoginModel { get; set; }
+
+        public IActionResult OnPost()
         {
+            var user = UserRepository.LoginUser(UserLoginModel);
+            if (user == null)
+            {
+                return RedirectToPage("/login");
+            }
+            else
+            {
+                Response.Cookies.Append("UserDetails", user.EmailAddress);
+                return RedirectToPage("/admin");
+            }
         }
     }
 }
