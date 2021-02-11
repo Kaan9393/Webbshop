2a5e3c158e49e8664b947540cf4638d530d7d6ce
Tonny <tonny035@hotmail.com>
2021-02-11 16:21:07 +0100

    authentication update

--- Kladbutiken/Pages/CategoryCrud/Index.cshtml.cs
+++ Kladbutiken/Pages/CategoryCrud/Index.cshtml.cs
@@ -7,23 +7,45 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using DataAccess.Data;
 using DataAccess.Entities;
+using DataAccess.Repositories;
 
 namespace Kladbutiken.Pages.CategoryCrud
 {
     public class IndexModel : PageModel
     {
         private readonly DataAccess.Data.MainContext _context;
+        private readonly IUserRepository _userRepository;
 
-        public IndexModel(DataAccess.Data.MainContext context)
+        public string LoggedInAs { get; set; }
+
+        public IndexModel(DataAccess.Data.MainContext context, IUserRepository userRepository)
         {
             _context = context;
+            _userRepository = userRepository;
         }
 
         public IList<Category> Category { get;set; }
 
-        public async Task OnGetAsync()
+        public async Task<IActionResult> OnGetAsync()
         {
             Category = await _context.Categories.ToListAsync();
+            var userDetailsCookie = Request.Cookies["UserDetails"];
+
+            if (userDetailsCookie == null)
+            {
+                return RedirectToPage("/login");
+            }
+
+            var user = _userRepository.GetUserByEmail(userDetailsCookie);
+
+            if (user.Role != "Admin")
+            {
+                return RedirectToPage("/index");
+            }
+
+            LoggedInAs = user.EmailAddress;
+
+            return Page();
         }
     }
 }
