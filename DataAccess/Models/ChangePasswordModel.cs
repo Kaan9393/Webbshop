fb2d9067f1c077ccf37367149c6e7b5abdc059a1
Tonny <tonny035@hotmail.com>
2021-03-02 18:13:53 +0100

    create PasswordModel

---
+++ DataAccess/Models/ChangePasswordModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class ChangePasswordModel
+    {
+        [Required(ErrorMessage = "Detta fältet krävs.")]
+        public string CurrentPassword { get; set; }
+
+        [Required(ErrorMessage = "Detta fältet krävs.")]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [Compare("NewPassword", ErrorMessage = "Lösenorden matchar inte.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
