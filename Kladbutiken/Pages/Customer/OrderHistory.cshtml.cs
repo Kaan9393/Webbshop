c3e4b60fa0bf864b40dcd917cefc967fd7b88c46
Kaan9393 <71064062+Kaan9393@users.noreply.github.com>
2021-03-03 10:45:52 +0100

    Orderhistorik

---
+++ Kladbutiken/Pages/Customer/OrderHistory.cshtml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess.Entities;
+using DataAccess.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Kladbutiken.Pages.Customer
+{
+    public class OrderHistoryModel : PageModel
+    {
+        private readonly IUserRepository _userRepository;
+
+        public User LoggedInAs { get; set; }
+
+        public OrderHistoryModel(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public void OnGet()
+        {
+            var userDetailsCookie = Request.Cookies["UserDetails"];
+            if (userDetailsCookie != null)
+            {
+                LoggedInAs = _userRepository.GetUserByEmail(userDetailsCookie);
+            }
+        }
+    }
+}
