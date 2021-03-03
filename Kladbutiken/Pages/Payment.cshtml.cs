5181dec2b126033529a255ddeeff25577ce79922
Kaan9393 <71064062+Kaan9393@users.noreply.github.com>
2021-03-03 13:18:51 +0100

    update

--- Kladbutiken/Pages/Payment.cshtml.cs
+++ Kladbutiken/Pages/Payment.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccess.Entities;
+using DataAccess.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,21 @@ namespace Kladbutiken.Pages
 {
     public class PaymentModel : PageModel
     {
+        public User LoggedInAs { get; set; }
+
+        private readonly IUserRepository _userRepository;
+
+        public PaymentModel(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
         public void OnGet()
         {
+            var userDetailsCookie = Request.Cookies["UserDetails"];
+            if (userDetailsCookie != null)
+            {
+                LoggedInAs = _userRepository.GetUserByEmail(userDetailsCookie);
+            }
         }
     }
 }
