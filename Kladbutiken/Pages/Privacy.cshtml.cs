c314d004012eb04b3acfb3307cf6c138b074d302
arkestal <71063982+arkestal@users.noreply.github.com>
2021-03-24 16:00:16 +0100

    Fixad footer

--- Kladbutiken/Pages/Privacy.cshtml.cs
+++ Kladbutiken/Pages/Privacy.cshtml.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using DataAccess.Entities;
+using DataAccess.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -12,14 +15,44 @@ namespace Kladbutiken.Pages
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
+        private readonly IUserRepository _userRepository;
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public List<Product> AllProducts { get; set; }
+        public List<Category> AllCategories { get; set; }
+        public List<Product> AllSelectedProducts { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SelectedCategory { get; set; }
+
         public User LoggedInAs { get; set; }
 
-        public PrivacyModel(ILogger<PrivacyModel> logger)
+        public PrivacyModel(ILogger<PrivacyModel> logger, IUserRepository userRepository, IProductRepository productRepository, ICategoryRepository categoryRepository)
         {
             _logger = logger;
+            _userRepository = userRepository;
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
         }
         public void OnGet()
         {
+            var userDetailsCookie = Request.Cookies["UserDetails"];
+            if (userDetailsCookie != null)
+            {
+                LoggedInAs = _userRepository.GetUserByEmail(userDetailsCookie);
+
+                var cart = HttpContext.Session.GetString("cart");
+                if (cart != null)
+                {
+                    LoggedInAs.ProductCart = _productRepository.GetProductsByList(JsonSerializer.Deserialize<List<Guid>>(cart));
+                }
+            }
+
+            _userRepository.CheckForAdmin();
+            AllProducts = _productRepository.GetAllProducts().ToList();
+            AllCategories = _categoryRepository.GetAllCategorys().ToList();
+            AllSelectedProducts = _productRepository.GetProductsByCategory(SelectedCategory).ToList();
         }
     }
 }
