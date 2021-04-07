ed738234365c7775b38061f98d5d058bd5f7fd43
Kaan9393 <71064062+Kaan9393@users.noreply.github.com>
2021-04-07 15:25:07 +0200

    Update

--- Kladbutiken/Pages/CategoryCrud/Details.cshtml.cs
+++ Kladbutiken/Pages/CategoryCrud/Details.cshtml.cs
@@ -8,6 +8,8 @@ using Microsoft.EntityFrameworkCore;
 using DataAccess.Data;
 using DataAccess.Entities;
 using DataAccess.Repositories;
+using Microsoft.AspNetCore.Http;
+using Kladbutiken.Utils;
 
 namespace Kladbutiken.Pages.CategoryCrud
 {
@@ -26,19 +28,23 @@ namespace Kladbutiken.Pages.CategoryCrud
 
         public async Task<IActionResult> OnGetAsync(Guid? id)
         {
-            var userDetailsCookie = Request.Cookies["UserDetails"];
-            var user = _userRepository.GetUserByEmail(userDetailsCookie);
-            LoggedInAs = user;
-            LoggedInAs.EmailAddress = user.EmailAddress;
-
             if (id == null)
             {
                 return NotFound();
             }
 
-            Category = await _context.Categories.FirstOrDefaultAsync(m => m.ID == id);
+            var cart = HttpContext.Session.GetString("cart");
+            var userDetailsCookie = Request.Cookies["UserDetails"];
+            if (userDetailsCookie == null)
+            {
+                return RedirectToPage("/login");
+            }
+            
 
-            if (Category == null)
+            //Category = await _context.Categories.FirstOrDefaultAsync(m => m.ID == id);
+            LoggedInAs = await UserCookieHandler.GetUserAndCartByCookies(userDetailsCookie, cart);
+
+            if (LoggedInAs.Role != "Admin")
             {
                 return NotFound();
             }
